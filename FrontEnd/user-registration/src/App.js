eea580ce6abea6052257f7b1e4ba2e5be12663f9
d-lysogorchiy <i.guruli.exceedteam@gmail.com>
2022-09-02 00:26:53 +0400

    FE-1 | Created new react app for front

---
+++ FrontEnd/user-registration/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserForm from './components/UserForm';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+    <h1>
+    Hello From react
+   </h1>
+   <UserForm />
+   </>
+   
+  );
+}
+
+export default App;
---
+++ FrontEnd/user-registration/src/components/UserForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+const UserForm = () => {
+
+
+    return (
+        <>
+<input></input>
+        </>
+    )
+
+}
+
+
+export default UserForm
